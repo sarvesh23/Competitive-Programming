4a3e4217f8705270739e4eea7ea1585df51e7e8e
Sarvesh Landge <42473906+sarvesh23@users.noreply.github.com>
2024-02-11 20:02:36 +0530

    Time: 2 ms (11.43%) | Memory: 41.4 MB (6.23%) - LeetSync

---
+++ leetcode/1635-number-of-good-pairs/number-of-good-pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=1;j<nums.length;j++)
+            {
+                if(i<j && nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
